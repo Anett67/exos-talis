c34612309faf9064db1ac6ea953b4b7351a7049b
Anett David <davidanett@gmail.com>
2024-10-24 12:21:16 +0200

    fix: exo 6

--- conditions-loops/exo_6.js
+++ conditions-loops/exo_6.js
@@ -1,13 +1,19 @@
-let number = 29
-let isPrime = true
-for (let i = 2; i < number; i++) {
-    if (number % i === 0) {
-        isPrime = false
-        break
+let number = 29;
+let isPrime = true;
+
+if (number <= 1) {
+    isPrime = false;
+} else {
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) {
+            isPrime = false;
+            break;
+        }
     }
 }
+
 if (isPrime) {
-    console.log(number + " est un nombre premier")
+    console.log(number + " est un nombre premier.");
 } else {
-    console.log(number + " n'est pas un nombre premier")
+    console.log(number + " n'est pas un nombre premier.");
 }
