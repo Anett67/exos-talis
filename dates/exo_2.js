faffd5e256adbeed404a51fd2ccce9049a84e5cd
DAVID Anett <a.david@talis-bs.net>
2024-10-28 11:42:46 +0100

    Add date solutions

---
+++ dates/exo_2.js
@@ -0,0 +1,15 @@
+// 2. Vérification de la validité d'une date
+function isValidDate(dateString) {
+    const date = new Date(dateString)
+    const year = date.getFullYear()
+    const month = addZero(date.getMonth() + 1)
+    const day = addZero(date.getDate())
+
+    console.log(`${year}-${month}-${day}` === dateString)
+}
+
+function addZero(number) {
+    return number < 10 ? `0${number}` : number
+}
+
+isValidDate('2023-02-29')
